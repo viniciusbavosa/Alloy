a49108dc530d5bf2e73662c891f2c41e2c81058a
viniciusbavosa <viniciusbavosa@outlook.com>
2024-10-14 08:30:37 -0300

    build: first Electron desktop window

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import { app, BrowserWindow } from "electron";
+
+// Creates and manages app windows
+// In Electron, BrowserWindows can only be created after the app module's ready event is fired.
+// You can wait for this event by using the app.whenReady() API and calling createWindow() once its promise is fulfilled.
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  win.loadFile('index.html');
+};
+
+// Initialize app (controls application's event lifecycle)
+app.whenReady()
+  .then(() => createWindow());
